88e8547b349c73967020d1cf0bda2dc9876004f0
Preston <pliger4@hotmail.com>
2021-03-16 22:09:43 -0400

    adding files

---
+++ src/components/Hero.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Button from 'react';
+import '../App.css';
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+    <div className='label-container'>
+      <div className='options'>
+      <h1>Chosen</h1>
+      <h2>Options:</h2>
+      <ul>
+      <i className='fas fa-cloud round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-temperature-high round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-thermometer round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-cloud round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-temperature-high round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-temperature-low round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-thermometer round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-cloud round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-thermometer round-icon'></i>
+      </ul>
+      <ul>
+      <i className='fas fa-cloud round-icon'></i>
+      </ul>
+      </div>
+      <div className='white'>
+      <h1>White Label</h1>
+      <h2>W-394900 XP</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p> 
+      <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+      <p>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+      <p>reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p> 
+      <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+      <p>deserunt mollit anim id est laborum.</p>
+      <span>LIVE VIEW</span>
+      </div>
+      <div className='Button'>
+      <button className='btn'>Order Now</button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+        <span className='nav-item'>
+              <Link
+                to='/'
+                className='nav-links'
+              >
+                Constructor
+              </Link>
+            </span>
+        <span className='nav-item'>
+          <Link
+            to='/Help'
+            className='nav-links'
+          >
+            Help
+          </Link>
+        </span>  
+        <span className='nav-item'>
+              <Link
+                to='/UsersPanels'
+                className='nav-links'
+              >
+                User Panels
+              </Link>
+            </span>
+        <span className='nav-item'>
+              <Link
+                to='/FAQ'
+                className='nav-links'
+              >
+                FAQ
+              </Link>
+            </span>
+        <span className='nav-item'>
+              <Link
+                to='/About'
+                className='nav-links'
+              >
+                About
+              </Link>
+            </span>
+        <span className='nav-item'>
+              <Link
+                to='/Blog'
+                className='nav-links'
+              >
+                Blog
+              </Link>
+            </span>
+            
+            <Link to="/" className="navbar-logo">
+            <i className='far fa-heart'></i>
+            <i className='fas fa-grip-lines-vertical'></i>
+            <i className='fas fa-search'></i>
+          </Link>
+        </div>
+      </nav>
+      <div className="chip-container">
+          <h1 class="text" style={{color: "#fff", padding: 10}}>Chip</h1><h1 class="text1">Constructor</h1>
+        </div>
+      <div className="icons-container">
+      <li>
+        <i className="fas fa-arrow-left"></i>
+      </li>
+      <li>
+      <a title="Cloud"><i className='fas fa-cloud circle-icon'></i><span>Cloud</span></a>
+      </li>
+      <li>
+      <a title="Temperature"><i className='fas fa-temperature-high circle-icon'></i><span>Water Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud Temp"><i className='fas fa-thermometer circle-icon'></i><span>Cloud Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud"><i className='fas fa-cloud circle-icon'></i><span>Dust Temp</span></a>
+      </li>
+      <li>
+      <a title="Temperature"><i className='fas fa-temperature-high circle-icon'></i><span>Temperature</span></a>
+      </li>
+      <li>
+      <a title="Temperature"><i className='fas fa-thermometer-empty circle-icon'></i><span>Water Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud Temp"><i className='fas fa-thermometer circle-icon'></i><span>Cloud Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud"><i className='fas fa-cloud circle-icon'></i><span>Dust Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud Temp"><i className='fas fa-thermometer circle-icon'></i><span>Cloud Temp</span></a>
+      </li>
+      <li>
+      <a title="Cloud"><i className='fas fa-cloud circle-icon'></i><span>Dust Temp</span></a>
+      </li>
+      <li>
+        <i className="fas fa-arrow-right"></i>
+      </li>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
